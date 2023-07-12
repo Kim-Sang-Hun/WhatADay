e1c58fcad4023f8036802b2b193ab33d183fca79
Sanghun77 <tpspfkdl@naver.com>
2023-07-12 14:34:52 +0900

    Initial Commit

---
+++ src/test/java/com/zerobase/whataday/WhatADayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.whataday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatADayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
