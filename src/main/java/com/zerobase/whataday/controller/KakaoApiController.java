a30e4546c04c66393896cdc2ec9d9136e22a8fd6
Sanghun77 <tpspfkdl@naver.com>
2023-07-26 20:27:21 +0900

    feat: 카카오 API 이용한 주소 검색

--- src/main/java/com/zerobase/whataday/controller/KakaoApiController.java
+++ src/main/java/com/zerobase/whataday/controller/KakaoApiController.java
@@ -1,40 +1,23 @@
 package com.zerobase.whataday.controller;
 
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+
+import com.zerobase.whataday.service.ApiService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequiredArgsConstructor
 public class KakaoApiController {
+  private final ApiService apiService;
 
   /**
    * 사용자가 검색한 address 값을 가지고 카카오 location API 사용하여 검색한 주소값, 위도, 경도를 return
    */
   @GetMapping("/search/{address}")
-  public ResponseEntity<?> searchAddress(@PathVariable("address") String address) throws Exception {
-    String apiKey = "KakaoAK " + "dcaa9a59bbcfc8f49ef3f4fc53648c4e";
-    String url = "https://dapi.kakao.com/v2/local/search/address.json?query=" + address;
-    RestTemplate restTemplate = new RestTemplate();
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
-    headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-    headers.set("Authorization", apiKey);
-
-    HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(headers);
-    ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, httpEntity,
-        String.class);
-
-    return responseEntity;
+  public ResponseEntity<String> searchAddress(@PathVariable("address") String address) {
+      return apiService.searchAddress(address);
   }
 }
