8a258b8535e223f758739d22be4cffb3bb8804f3
Sanghun77 <tpspfkdl@naver.com>
2023-08-12 15:55:08 +0900

    feat : gitignore 변경

--- src/test/java/com/zerobase/whataday/WhatADayApplicationTests.java
+++ src/test/java/com/zerobase/whataday/WhatADayApplicationTests.java
@@ -1,13 +1,8 @@
 package com.zerobase.whataday;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WhatADayApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
